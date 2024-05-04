14f5a667861ae4504890d842f7fcd67b9a5b18e6
myanpetra99 <myanpetra99@gmail.com>
2024-05-05 01:33:47 +0700

    add hello name

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = express();
 
 app.get('/', (req, res) => {
  
+    if (req.query.name) {
+        return res.send(`Hello ${req.query.name}!`);
+    }
+
     res.send('Hello World!');
   
 });
 
 const server = app.listen(3000, () => console.log(`Server running on port 3000`));
 
-module.exports = server;  // Export for testing
+module.exports = server; 
