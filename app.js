1491ec8a5a9d5d35ac17e5933d909cef28a66e78
myanpetra99 <myanpetra99@gmail.com>
2024-05-04 21:36:10 +0700

    add hello name

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+
+    if (req.query.name) {
+        return res.send(`Hello ${req.query.name}!`);
+    }
  
     res.send('Hello World!');
   
--- app.test.js
+++ app.test.js
@@ -8,6 +8,12 @@ describe('GET /', () => {
     expect(response.statusCode).toBe(200);
   });
 
+  it('responds with Hello {name}! (capital name)', async () => {
+    const response = await request(server).get('/?name=John');
+    expect(response.text).toEqual('Hello JOHN!');
+    expect(response.statusCode).toBe(200);
+  });
+
   afterAll(done => {
     server.close(done);
   });
